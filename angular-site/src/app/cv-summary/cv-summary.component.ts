f6cdc97d741bdfb2e4abc2e039ffecb182a467a5
Seth Niemuth <seth.niemuth@controlf1.co.uk>
2017-03-14 10:05:32 +0000

    A bit of styling to make it nicer

--- angular-site/src/app/cv-summary/cv-summary.component.ts
+++ angular-site/src/app/cv-summary/cv-summary.component.ts
@@ -18,14 +18,14 @@ export class CvSummaryComponent implements OnInit {
   name = "Seth Niemuth";
   email = "seth@madebyseth.com";
   skype = "sniemuth";
-  profileHighlights = ["Over 9 years' experience developing in the Microsoft stack", "Worked across a range of business industries and public sector projects", "Comfortable as sole developer or collaborating in team", "All around programmer: strong front-end and middle tier skills along with database experience", "Motivated to create clean, quality, tested code using modern standards", "Experience in waterfall and agile methodologies", " Worked with most major source control systems", "Adaptable, focused, highly efficient developer with passion for excellence"];
-  // These should be coming from a database somewhere
+  profileHighlights = ["Over 9 years' experience developing in the Microsoft stack", "Worked across a range of business industries and public sector projects", "Comfortable as sole developer or collaborating in team", "All around programmer: strong front-end and middle tier skills along with database experience", "Motivated to create clean, quality, tested code using modern standards", "Experience in waterfall and agile methodologies", "Keen and quick learner who is flexible to change", "Adaptable, focused, highly efficient developer with passion for excellence"];
+  // Ideally these would be coming from an API and stored in a database
   technicalSkillsMatrix: Array<CvSummarySection> = [
-    { name: "Microsoft", technologies: ["C#", ".NET 4.x", "MVC", "MS SQL", "Entity Framework", "WebAPI", "WCF", "SignalR", "LINQ"] },
-    { name: "Cloud", technologies: ["AWS", "Elastic Beanstalk", "DynamoDB", "Lambda", "SNS", "SQS"] },
-    { name: "Front-end", technologies: ["Angular2", "KnockoutJS", "HTML", "CSS", "Javascript", "jQuery", "less", "KendoUI", "XSLT", "Adobe products (Fireworks, Photoshop)"] },
+    { name: "Microsoft", technologies: ["C#", ".NET 4.x", "MVC", "MS SQL", "Entity Framework", "WebAPI", "WCF/Web Services", "SignalR", "LINQ", "SSIS"] },
+    { name: "Cloud", technologies: ["AWS", "Azure Web App", "Elastic Beanstalk", "DynamoDB", "Lambda", "SNS", "SQS"] },
+    { name: "Front-end", technologies: ["Angular2", "KnockoutJS", "Javascript", "jQuery", "less", "KendoUI"] },
     { name: "Utilities/CMS", technologies: ["RabbitMQ", "Postsharp", "Automapper", "umbraco", "nopCommerce"] },
-    { name: "CI and versioning", technologies: ["TeamCity", "TFS", "CruiseControl", "SVN", "Git/BitBucket"] },
+    { name: "CI and versioning", technologies: ["TeamCity", "TFS", "CruiseControl", "SVN", "Git"] },
     { name: "IoC/Testing", technologies: ["Ninject", "Unity", "Castle", "TDD", "BDD", "NUnit", "Moq", "NSubstitute", "Selenium", "SpecFlow"] },
     { name: "PM Methodologies", technologies: ["Agile Kanban", "Agile Scrum", "Waterfall"] },
     { name: "Monitoring/Tracking Tools", technologies: ["TargetProcess", "Confluence", "JIRA", "TFS"] },
@@ -42,6 +42,7 @@ export class CvSummaryComponent implements OnInit {
     this.selectedTechnology = null;
   }
 
+  // Turn on and off the technology selector
   onTechnologySelect(technology: string): void {
     // Turn off the selected
     if (this.selectedTechnology === technology) {
--- angular-site/src/app/work-record.service.ts
+++ angular-site/src/app/work-record.service.ts
@@ -7,30 +7,31 @@ import { WorkRecord } from './work-record';
 @Injectable()
 export class WorkRecordService {
   private workRecordUrl = 'http://madebysethcvapiweb20170311075832.azurewebsites.net/api/workrecords';  // URL to web api
+  // private workRecordUrl = "http://localhost:56807/api/workrecords";
 
   constructor(private http: Http) { }
 
+  // Use the Promise from rxjs for the http call to not block
   getWorkRecords(): Promise<WorkRecord[]> {
     return this.http.get(this.workRecordUrl)
-               .toPromise()
-               .then(               
-                   response => response.json() as WorkRecord[]
-               )
-               .catch(this.handleError);
+      .toPromise()
+      .then(
+      response => response.json() as WorkRecord[]
+      )
+      .catch(this.handleError);
   }
 
+  // Handle error
   private handleError(error: any): Promise<any> {
     console.error('An error occurred', error); // for demo purposes only
     return Promise.reject(error.message || error);
   }
 
   getWorkRecord(id: number): Promise<WorkRecord> {
-  const url = `${this.workRecordUrl}/${id}`;
-  return this.http.get(url)
-    .toPromise()
-    .then(response => response.json().data as WorkRecord)
-    .catch(this.handleError);
-}
-
-
+    const url = `${this.workRecordUrl}/${id}`;
+    return this.http.get(url)
+      .toPromise()
+      .then(response => response.json().data as WorkRecord)
+      .catch(this.handleError);
+  }
 }
